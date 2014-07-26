d45235cdbceef2e50346f265d5dacbe60b56e2c4
thunderstormwang <thunderstormwang@gmail.com>
2014-07-26 21:59:06 +0800

    套版

---
+++ Workshop/Controllers/ArticleController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Workshop.Controllers
+{
+    public class ArticleController : Controller
+    {
+        // GET: Article
+        public ActionResult Hot()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        public ActionResult Details()
+        {
+            return View();
+        }
+    }
+}
